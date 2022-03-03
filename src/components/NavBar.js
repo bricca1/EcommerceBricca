46d692640d9242fdcfbb07ee783d0c5bdc549a7a
nicolas.bricca1 <nicolas.bricca@silstech.com.ar>
2022-03-03 18:34:05 -0300

    mejora icono

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,11 +2,12 @@
 import CartWidget from './CartWidget.js'
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Button from '@mui/material/Button';
+import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
 
 function NavBar(){
     return<>
         <div className="App-spacer">
-            <h3>CELLFIX</h3>
+            <h3>CELLFIX <PhoneIphoneIcon/></h3>
             <ButtonGroup variant="contained" aria-label="outlined primary button group" fullWidth>
                 <Button><a href="">
                     Productos 
